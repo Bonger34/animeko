4bc3c6080237ee93c97fcc97142004ff05c78b1c
Bonger34 <2700875818@qq.com>
2026-08-29 00:21:32 +0800

    fix: use debug signing for release builds

--- app/android/build.gradle.kts
+++ app/android/build.gradle.kts
@@ -91,7 +91,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
-            signingConfig = null
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 *sharedAndroidProguardRules(),
